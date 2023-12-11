1284d123eb69ba27a668dc0c28e317ba7e2f3e4e
pdksJava <112490909+pdksJava@users.noreply.github.com>
2023-12-11 19:47:39 +0300

    Delete pdks-web directory

--- pdks-web/WebContent/img/scriptFile.js
+++
@@ -1,7 +0,0 @@
-function updatePanelPassword(form) {
-	
-	var formName= form.id;
-    var checkbox = document.getElementById(formName+":ldapuse");
-    var div = document.getElementById(formName+":userEdit:optionsPanel");
-    div.style.visibility = checkbox.checked ? "hidden" : "visible";
-}
